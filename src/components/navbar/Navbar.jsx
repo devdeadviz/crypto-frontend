cfc578309dbf61eba8f3bc54475427582bed4f1c
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-07-01 17:46:55 +0530

    feat: add navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { Box, IconButton, Image } from '@chakra-ui/react';
+import { ColorModeSwitcher } from '../../ColorModeSwitcher';
+import { SearchIcon, HamburgerIcon } from '@chakra-ui/icons';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <Box
+      as="nav"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      p={2}
+    >
+      <Image src="/assets/logo.svg" />
+      <Box display="flex">
+        <IconButton
+          aria-label="Search database"
+          bg="none"
+          icon={<SearchIcon />}
+        />
+        <IconButton aria-label="Hamburger" bg="none" icon={<HamburgerIcon />} />
+        <ColorModeSwitcher />
+      </Box>
+    </Box>
+  );
+};
+
+export { Navbar };
