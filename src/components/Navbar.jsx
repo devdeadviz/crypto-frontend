5135a68ce4a91ba4facda9ee971aa8fd4967950f
Kuldeep Gupta <kuldeepgupta485@gmail.com>
2022-07-03 23:12:40 +0530

    styles: add responsivess

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { Box, Flex, IconButton, Image } from '@chakra-ui/react';
+import { Box, Flex, Hide, IconButton, Image } from '@chakra-ui/react';
 import { SearchIcon, HamburgerIcon } from '@chakra-ui/icons';
 
 const Navbar = () => {
   return (
     <Flex
       alignItems="center"
-      justifyContent="center"
+      justifyContent={['flex-start', 'center']}
       bg="#FFFFFF"
       borderBottomColor="#E5E5E5"
     >
@@ -13,18 +13,21 @@ const Navbar = () => {
         as="nav"
         display="flex"
         alignItems="center"
-        justifyContent="space-between"
+        justifyContent={[null, 'space-between']}
+        flexDirection={['row-reverse', 'row']}
         p={2}
         maxWidth="228.3rem"
-        width="90%"
+        width={[null, '90%']}
       >
         <Image src="/assets/logo.svg" />
         <Box display="flex">
-          <IconButton
-            aria-label="Search database"
-            bg="none"
-            icon={<SearchIcon />}
-          />
+          <Hide breakpoint="(max-width: 780px)">
+            <IconButton
+              aria-label="Search database"
+              bg="none"
+              icon={<SearchIcon />}
+            />
+          </Hide>
           <IconButton
             aria-label="Hamburger"
             bg="none"
